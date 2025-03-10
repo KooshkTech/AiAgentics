553010d1eb42169955889f1bed0987caa868beee
KooshkTech <kooshktech@gmail.com>
2025-03-10 12:32:45 +0200

    Update app.py
    code

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+rom langgraph.graph import StateGraph, START, END
+from typing_extensions import TypedDict
 
+from pydantic import BaseModel
+
+
+# The overall state of the graph (this is the public state shared across nodes)
+class OverallState(BaseModel):
+    a: str
+
+def node(state:OverallState):
+    return {"a":"Hi I am Krish"}

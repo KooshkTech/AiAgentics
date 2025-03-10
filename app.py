b631c05d911f771482580d64f58a0504c6782ec4
KooshkTech <kooshktech@gmail.com>
2025-03-10 13:48:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-rom langgraph.graph import StateGraph, START, END
-from typing_extensions import TypedDict
-
-from pydantic import BaseModel
+from src.langgraphagenticai.main import load_langgraph_agenticai_app
 
 
-# The overall state of the graph (this is the public state shared across nodes)
-class OverallState(BaseModel):
-    a: str
-
-def node(state:OverallState):
-    return {"a":"Hi I am Krish"}
+if __name__=="__main__":
+    load_langgraph_agenticai_app()
